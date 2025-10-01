0eebd457676a19b2bbaa1250092318d19a78bd08
Yohan Chalier <yohan@chalier.fr>
2025-10-01 11:09:54 +0200

    liquid-vapor: cleanup and additionnal features

--- liquid-vapor/master.js
+++ liquid-vapor/master.js
@@ -1,17 +1,9 @@
-var width = window.innerWidth;
-var height = window.innerHeight;
-var T = 0.5;
-var C = 0;
-var imageData = new ImageData(width, height);
-var context;
-var alpha = .8;
-var N = 0;
-var noise = 1 / width / height;
-
-canvas.width = width;
-canvas.height = height;
-
+const SPEEDS = [0.2, 0.5, 0.8, 0.9, 0.99, 0.999];
 const gl = canvas.getContext("webgl2", {preserveDrawingBuffer: true});
+var speedIndex = 1;
+var previousTime = 0;
+var pastBuffer;
+var nextBuffer;
 
 function createEmptyArray(arrayWidth, arrayHeight) {
     const array = [];
@@ -63,7 +55,8 @@ varying vec2 uv;
 void main() {
     uv = (position * 0.5) + 0.5;
     gl_Position = vec4(position, 0, 1);
-}`);
+}
+`);
 
 const simulationShader = createShader(gl.FRAGMENT_SHADER, `
 precision highp float;
@@ -74,109 +67,81 @@ varying vec2 uv;
 uniform float u_time;
 uniform float u_dt;
 uniform float u_speed;
-uniform float width;
-uniform float height;
-uniform float T;
-uniform float C;
+uniform float u_width;
+uniform float u_height;
+uniform float u_temp;
+uniform float u_pot;
 
-float rand(vec2 co) {
-    return fract(sin(dot(co.xy ,vec2(132.9898, 478.233))) * 43758.5453);
+const float lifeSpan = 3.0; // seconds
+
+float rand(vec2 coords) {
+    return fract(sin(dot(coords.xy, vec2(12.9898, 78.233))) * 43758.5453);
 }
 
-float spinAt(vec2 offset) {
-    vec2 coords = uv + offset / vec2(width, height);
+float spinat(float dx, float dy) {
+    vec2 coords = uv + vec2(dx, dy) / vec2(u_width, u_height);
     vec4 color = texture2D(sampler, coords);
     return color.w > 0.5 ? 1.0 : -1.0;
 }
 
 void main() {
-    float S = spinAt(vec2(0.9, 0.0)) + spinAt(vec2(-0.9, 0.0)) + spinAt(vec2(0.0, 0.9)) + spinAt(vec2(0.0, -0.9));
-    float spinXY = spinAt(vec2(0.0, 0.0));
-    float deltaE = 2.0 * spinXY * (S + C);
-    float proba = exp(-deltaE / T);
+    float spin_neighbors = spinat(1.0, 0.0) + spinat(-1.0, 0.0) + spinat(0.0, 1.0) + spinat(0.0, -1.0);
+    float spin_center = spinat(0.0, 0.0);
+    float energy = 2.0 * spin_center * (spin_neighbors + u_pot);
+    float p = exp(-energy / u_temp);
     float r1 = rand(uv + u_time + 0.0);
     float r2 = rand(uv + u_time + 1.0);
-    if (r1 > u_speed && (deltaE < 0.0 || r2 < proba)) {
-        spinXY *= -1.0;
+    if (r1 > u_speed && (energy < 0.0 || r2 < p)) {
+        spin_center *= -1.0;
     }
-    if (spinXY < 0.0) {
-        gl_FragColor.x = 0.0;
-    } else {
-        float oldX = texture2D(sampler, uv).x;
-        gl_FragColor.x = oldX + u_dt / 3.0;
-    }
-    gl_FragColor.w = floor((spinXY + 1.0) / 2.0);
-}`);
+    gl_FragColor.x = spin_center < 0.0 ? 0.0 : min(1.0, texture2D(sampler, uv).x + u_dt / lifeSpan);
+    gl_FragColor.w = floor((spin_center + 1.0) / 2.0);
+}`
+);
 
 const copyShader = createShader(gl.FRAGMENT_SHADER, `
 precision highp float;
+
 uniform sampler2D sampler;
 varying vec2 uv;
+
 void main() {
     gl_FragColor = texture2D(sampler, uv);
-}`);
+}
+`);
 
 const colorShader = createShader(gl.FRAGMENT_SHADER, `
 precision highp float;
+
 uniform sampler2D sampler;
 varying vec2 uv;
+
+const vec3 youngColor = vec3(0.812, 0.941, 0.063);
+const vec3 oldColor   = vec3(0.063, 0.937, 0.451);
+const vec3 bgColor    = vec3(0.063, 0.063, 0.063);
+
 void main() {
     vec4 color = texture2D(sampler, uv);
-    float age = color.x;
-    float alpha = color.w;
-    if (alpha > 0.5) {
-        vec3 startColor = vec3(0.812, 0.941, 0.063);
-        vec3 endColor = vec3(0.063, 0.937, 0.451);
-        gl_FragColor = vec4(mix(startColor, endColor, age), 1.0);
-    } else {
-        gl_FragColor = vec4(0.063, 0.063, 0.063, 1.0);
-    }
-}`);
+    gl_FragColor.xyz = color.w > 0.5 ? mix(youngColor, oldColor, color.x) : bgColor;
+    gl_FragColor.w = 1.0;
+}
+`);
 
 const simulationProgram = createProgram(vertexShader, simulationShader);
 const copyProgram = createProgram(vertexShader, copyShader);
 const colorProgram = createProgram(vertexShader, colorShader);
 
-const tLocation = gl.getUniformLocation(simulationProgram, "T");
-const cLocation = gl.getUniformLocation(simulationProgram, "C");
-const wLocation = gl.getUniformLocation(simulationProgram, "width");
-const hLocation = gl.getUniformLocation(simulationProgram, "height");
+const uTemperature = gl.getUniformLocation(simulationProgram, "u_temp");
+const uChemicalPotential = gl.getUniformLocation(simulationProgram, "u_pot");
+const uWidth = gl.getUniformLocation(simulationProgram, "u_width");
+const uHeight = gl.getUniformLocation(simulationProgram, "u_height");
 const uTime = gl.getUniformLocation(simulationProgram, "u_time");
 const uDt = gl.getUniformLocation(simulationProgram, "u_dt");
 const uSpeed = gl.getUniformLocation(simulationProgram, "u_speed");
 
-const pastTexture = gl.createTexture();
-initializeTexture(pastTexture);
-gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.UNSIGNED_BYTE, createEmptyArray(width, height));
-const pastBuffer = gl.createFramebuffer();
-gl.bindFramebuffer(gl.FRAMEBUFFER, pastBuffer);
-gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, pastTexture, 0);
-
-const nextTexture = gl.createTexture();
-initializeTexture(nextTexture);
-gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.UNSIGNED_BYTE, createEmptyArray(width, height));
-const nextBuffer = gl.createFramebuffer();
-gl.bindFramebuffer(gl.FRAMEBUFFER, nextBuffer);
-gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, nextTexture, 0);
-
-gl.useProgram(simulationProgram);
-initializeBuffer();
-bindTexture(simulationProgram, "sampler", 0, pastTexture);
-
-gl.useProgram(copyProgram);
-initializeBuffer();
-bindTexture(copyProgram, "sampler", 1, nextTexture);
-
-gl.useProgram(colorProgram);
-initializeBuffer();
-bindTexture(colorProgram, "sampler", 1, nextTexture);
-
-gl.useProgram(simulationProgram);
-gl.uniform1f(tLocation, 1);
-gl.uniform1f(cLocation, 0);
-window.addEventListener("mousemove", (event) => {
-    T = Math.pow(10, event.clientX / window.innerWidth * 2 - 1);
-    C = 4 * (event.clientY / window.innerHeight - .5);
+function updateParameters(x, y) {
+    let T = Math.pow(10, x * 2 - 1);
+    let C = 4 * (y - .5);
     if (Math.abs(C) < 0.2) {
         C = 0;
     } else if (C > 0) {
@@ -185,51 +150,155 @@ window.addEventListener("mousemove", (event) => {
         C += 0.2;
     }
     gl.useProgram(simulationProgram);
-    gl.uniform1f(tLocation, T);
-    gl.uniform1f(cLocation, C);
+    gl.uniform1f(uTemperature, T);
+    gl.uniform1f(uChemicalPotential, C);
+}
+
+window.addEventListener("mousemove", (event) => {
+    updateParameters(event.clientX / window.innerWidth, event.clientY / window.innerHeight);
 });
 
-const speeds = [0.2, 0.5, 0.8, 0.9, 0.99, 0.999];
-var speed = 2;
-gl.useProgram(simulationProgram);
-gl.uniform1f(uSpeed, speeds[speed]);
+window.addEventListener("touchstart", (event) => {
+    const touches = event.changedTouches;
+    if (touches.length > 0) {
+        updateParameters(touches[0].pageX / window.innerWidth, touches[0].pageY / window.innerHeight);
+    }
+});
+
+window.addEventListener("touchmove", (event) => {
+    const touches = event.changedTouches;
+    if (touches.length > 0) {
+        updateParameters(touches[0].pageX / window.innerWidth, touches[0].pageY / window.innerHeight);
+    }
+});
+
 window.addEventListener("wheel", (event) => {
     if (event.deltaY > 0) {
-        speed++;
+        speedIndex++;
     } else {
-        speed--;
+        speedIndex--;
     }
-    speed = Math.max(0, Math.min(speeds.length - 1, speed));
+    speedIndex = Math.max(0, Math.min(SPEEDS.length - 1, speedIndex));
     gl.useProgram(simulationProgram);
-    gl.uniform1f(uSpeed, speeds[speed]);
+    gl.uniform1f(uSpeed, SPEEDS[speedIndex]);
 });
 
-var oldTime = 0;
-function render(time) {
+function setup() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+    canvas.width = width;
+    canvas.height = height;
+
+    const pastTexture = gl.createTexture();
+    initializeTexture(pastTexture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.UNSIGNED_BYTE, createEmptyArray(width, height));
+    pastBuffer = gl.createFramebuffer();
+    gl.bindFramebuffer(gl.FRAMEBUFFER, pastBuffer);
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, pastTexture, 0);
+
+    const nextTexture = gl.createTexture();
+    initializeTexture(nextTexture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.UNSIGNED_BYTE, createEmptyArray(width, height));
+    nextBuffer = gl.createFramebuffer();
+    gl.bindFramebuffer(gl.FRAMEBUFFER, nextBuffer);
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, nextTexture, 0);
 
     gl.useProgram(simulationProgram);
-    gl.uniform1f(wLocation, width);
-    gl.uniform1f(hLocation, height);
-    gl.uniform1f(uTime, time * 0.001);
-    gl.uniform1f(uDt, (time - oldTime) * 0.001);
+    initializeBuffer();
+    bindTexture(simulationProgram, "sampler", 0, pastTexture);
+    gl.uniform1f(uWidth, width);
+    gl.uniform1f(uHeight, height);
+    gl.uniform1f(uSpeed, SPEEDS[speedIndex]);
+    updateParameters(0.5, 0.5);
+
+    gl.useProgram(copyProgram);
+    initializeBuffer();
+    bindTexture(copyProgram, "sampler", 1, nextTexture);
+
+    gl.useProgram(colorProgram);
+    initializeBuffer();
+    bindTexture(colorProgram, "sampler", 1, nextTexture);
+
     gl.viewport(0, 0, width, height);
+}
+
+function render(time) {
+    gl.useProgram(simulationProgram);
+    gl.uniform1f(uTime, time * 0.001);
+    gl.uniform1f(uDt, (time - previousTime) * 0.001);
     gl.bindFramebuffer(gl.FRAMEBUFFER, nextBuffer);
     gl.drawArrays(gl.TRIANGLES, 0, 6);
 
     gl.useProgram(copyProgram);
-    gl.viewport(0, 0, width, height);
     gl.bindFramebuffer(gl.FRAMEBUFFER, pastBuffer);
     gl.drawArrays(gl.TRIANGLES, 0, 6);
 
     gl.useProgram(colorProgram);
-    gl.viewport(0, 0, width, height);
     gl.bindFramebuffer(gl.FRAMEBUFFER, null);
     gl.drawArrays(gl.TRIANGLES, 0, 6);
-    
-    oldTime = time;
 
+    previousTime = time;
     requestAnimationFrame(render);
-
 }
 
+window.addEventListener("resize", setup);
+
+function screenshot() {
+    const link = document.createElement("a");
+    const fileName = `liquid-vapor-${parseInt((new Date()) * 1)}.png`;
+    link.setAttribute("download", fileName);
+    link.href = canvas.toDataURL("image/png");
+    link.click();
+}
+
+var isRecording = false;
+var recordedChunks = [];
+var mediaRecorder = null;
+
+function startRecording() {
+    if (isRecording) return;
+    recordIcon.classList.remove("hidden");
+    isRecording = true;
+    recordedChunks = [];
+    return new Promise(function (res, rej) {
+        var stream = canvas.captureStream(30);
+        mediaRecorder = new MediaRecorder(stream, {
+            mimeType: "video/webm;codecs:vp9",
+            videoBitsPerSecond: 20000000
+        });
+        mediaRecorder.start(4000);
+        mediaRecorder.ondataavailable = function (event) {
+            recordedChunks.push(event.data);
+        }
+        mediaRecorder.onstop = function (event) {
+            var blob = new Blob(recordedChunks, {type: "video/webm" });
+            var a = document.createElement("a");
+            a.setAttribute("download", `liquid-vapor-${parseInt((new Date()) * 1)}.webm`);
+            a.href = URL.createObjectURL(blob);
+            a.click();
+        }
+    });
+}
+
+function stopRecording() {
+    if (!isRecording) return;
+    recordIcon.classList.add("hidden");
+    isRecording = false;
+    mediaRecorder.stop();
+}
+
+window.addEventListener("keydown", (event) => {
+    if (event.key == "s") {
+        screenshot();
+    } else if (event.key == "r") {
+        if (!isRecording) {
+            startRecording();
+        } else {
+            stopRecording();
+        }
+    }
+});
+
+setup();
 render();
